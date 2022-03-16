f4e180adb893db9fbdb83c9a0245368c53dfad11
Nadia <huisjes@biochem.mpg.de>
2022-03-16 16:20:23 +0100

    add get_acceptor_donor_bleach_fret()

--- marspylib/fret/__init__.py
+++ marspylib/fret/__init__.py
@@ -50,3 +50,51 @@ def get_T_bleach(molecule, metadata_tag_populations = ['FRET', 'AO', 'DO'], name
         T_bleach = np.NaN
 
     return T_bleach
+
+
+
+def get_acceptor_donor_bleach_fret(molecule, metadata_tag_fret = 'FRET', names_bleaching_events = ['Donor_Bleach', 'Acceptor_Bleach']):
+    '''
+    Function that returns the T_bleach position for a molecule. IMPORTANT: both bleaching positions are only retrieved
+        in the case the molecule has a metadata tag representing a FRET molecule.
+
+    Requirements
+    archive: an archive should have been initiated prior to running this function.
+
+    Inputs
+    molecule: the variable molecule, representing a single molecule record in the archive, should
+        have been defined prior to running this function. By default set to the name molecule.
+    metadata_tag_fret: default 'FRET', string denoting the tags present in the archive to tag molecules displaying
+        FRET behavior.
+    names_bleaching_events: default ['Donor_Bleach', 'Acceptor_Bleach'], list with strings denoting the position
+        names of the donor and bleaching events in the archive.
+        Note: names have to be entered in the specific order (Donor bleaching name, Acceptor bleaching name).
+
+    Outputs
+    T_bleach: the T-position of the bleaching point where either one of the dyes (donor or acceptor) has bleached.
+        Numerical value.
+
+    @Author: Nadia M. Huisjes
+
+    '''
+
+    if (archive.metadataHasTag(molecule.getMetadataUID(),metadata_tag_fret)):
+                if (molecule.hasPosition(names_bleaching_events[1]) & molecule.hasPosition(names_bleaching_events[0])):
+                    T_AO_bleach = molecule.getPosition(names_bleaching_events[1]).getPosition()
+                    T_DO_bleach = molecule.getPosition(names_bleaching_events[0]).getPosition()
+
+                    if int(T_AO_bleach) < int(T_DO_bleach):
+                        T_bleach = int(T_AO_bleach)
+                        T_second_bleach = int(T_DO_bleach)
+                        dye = ['acceptor']
+                    else:
+                        T_bleach = int(T_DO_bleach)
+                        T_second_bleach = int(T_AO_bleach)
+                        dye = ['donor']
+
+                else:
+                    T_bleach = np.NaN
+                    T_second_bleach = np.NaN
+                    dye = ['NaN']
+
+    return (T_bleach, T_second_bleach, dye)
