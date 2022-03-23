a978df270d115d393de36dd0be215d51d5fed38c
NadiaHuisjes <nadiahuisjes@gmail.com>
2022-03-23 11:56:50 +0100

    additions to the setup file for a more complete project description

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 **Mars** - **M**olecule **AR**chive **S**uite
 
+**marspylib**
 Python library with utility functions and converters for working with mars.
--- setup.py
+++ setup.py
@@ -8,7 +8,25 @@ setup(
     description='Library containing python functions to interact with Mars Archives',
     author='Karl Duderstadt, Nadia Huisjes, Thomas Retzer',
     url='https://github.com/duderstadt-lab/marspylib',
-    install_requires=[],
+    download_url='https://pypi.org/project/marspylib/',
+    project_urls={
+        'Documentation': 'https://duderstadt-lab.github.io/mars-docs/'},
+    long_description=open(README.md).read(),
+    long_description_content_type='text/markdown',
+
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'imagej',
+        'jpype',
+        'scyjava',
+        'itertools',
+        'java.io',
+        'scipy',
+        'pylab'
+    ],
     setup_requires=['pytest-runner'],
     tests_requires=['pytest'],
     test_suite='tests'
