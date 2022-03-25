0ecf8054e3de6774dc467910d78bce5fd115d2c1
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-03-25 09:00:24 +0100

    Add unit test for figure_to_imgsrc function

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,8 +1,17 @@
 import pytest
 import marspylib
-
+import matplotlib.pyplot as plt
 
 ## To add: unit tests figure_to_imgsrc(), gauss()
+
+def test_figure_to_imgsrc():
+    '''Test if the generated figure is correctly converted to an img src data string'''
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    fig = plt.gcf()
+    fig.set_size_inches(0.1, 0.1)
+    fig.set_dpi(50)
+    assert figure_to_imgsrc(fig) == 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjUuMSwgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/YYfK9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAlklEQVR4nHWOIQrCYABG32+w/EkwCAtDMCwq/00sHsLugmcYiHcwGSxL6hF+g7YNDzDQ5IqK8zPoxOKrDx7PSOIfBvDWWhdFEQCn8kZxuXIvjmck4ZyTJM23ucI41XixE+AbdSJZZySbnOEgYDbqf9O+FXQVxqkmy70e1VN6j3gk0ez0NF0dVH1ELY0kjDElkP2MtgH7Ap5xTn0Bv6XKAAAAAElFTkSuQmCC'
 
 def test_flatten():
     '''Test if the test list returns the correct flattened list'''
