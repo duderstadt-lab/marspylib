484ea1256bbd9091c035d160e8195e262aaff4df
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-04-21 10:37:47 +0200

    Add bin with launchFiji executable script
    add to setup.py to ensure installation

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     name='marspylib',
     packages=find_packages(),
     version="0.1.1",
-    description='Library containing python functions to interact with Mars Archives',
+    description='Library containing python Fiji launcher and python functions to interact with Mars Archives',
     author='Karl Duderstadt, Nadia Huisjes, Thomas Retzer',
     url='https://github.com/duderstadt-lab/marspylib',
     install_requires=[],
+    scripts=['bin/launchFiji'],
     setup_requires=['pytest-runner'],
     tests_requires=['pytest'],
     test_suite='tests'
