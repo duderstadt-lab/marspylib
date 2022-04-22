3092bec946d784ab8ac22f660c972d12cb044ad1
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-04-22 19:17:33 +0200

    bump version, clean-up setup

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ from os import path
 setup(
     name='marspylib',
     packages=find_packages(),
-    version="0.1.2",
+    platforms=['any'],
+    version="0.1.3",
     description='Library containing python Fiji launcher and python functions to interact with Mars Archives',
     author='Karl Duderstadt, Nadia Huisjes, Thomas Retzer',
     url='https://github.com/duderstadt-lab/marspylib',
-    install_requires=[],
+    install_requires=[
+      'jpype1 >=1.3.0',
+      'pyimagej',
+      'scyjava',
+      'matplotlib'
+    ],
     scripts=['bin/launchFiji'],
-    tests_requires=['pytest'],
-    test_suite='tests'
+    tests_requires=['pytest']
 )
